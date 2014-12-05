7b235a79998085b52eb35a29de721bafd729d1db
Prelang Builder <builder@prelang.com>
2014-12-05 20:48:03 +0000

    Generated controller for root route with 'index' action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
   resources :workout_posts
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
